8e1f4b9098e7e192b9fa59cc4c74bd18efd70b64
Max Bartolo <maxbartolo@gmail.com>
2017-12-06 14:55:35 +0000

    Updated ["text"] to ['answerText']

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -6,7 +6,7 @@ def test_answer(cc):
     token = cc.get_user_token()
     answers = cc.answer(token, "Is this API easy to use?")
     assert len(answers) == 1
-    assert answers[0]["text"] is not None
+    assert answers[0]['answerText'] is not None
 
 
 def test_multiple_answers(cc):
