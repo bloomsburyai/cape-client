70edb248d750754890268bb090041e84bfe472df
Luis Ulloa <timken@gmail.com>
2017-12-13 16:28:56 +0000

    Adding install deps

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+import sys
+import pip
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
+
+# TODO is there a better way ? dependency_links seems to be deprecated and to require a version
+if "--no-deps" in sys.argv:
+    sys.argv.remove("--no-deps")
+    print(f"Skipped dependencies of {NAME}", file=sys.stderr)
+else:
+    command = pip.main(['install', '--upgrade', '--ignore-installed', '-r', 'requirements.txt'])
+    if command > 0:
+        print(f"Detected error while installing dependencies abandoning install of {NAME}", file=sys.stderr)
+        sys.exit(command)
 
 setup(
     name=NAME,
