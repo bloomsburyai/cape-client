e0ffbeac493e8b77910fe6d1b0bc57edd7fc4b3d
Luis Ulloa <timken@gmail.com>
2018-01-05 17:57:32 +0000

    Updated client

--- cape/client/client.py
+++ cape/client/client.py
@@ -58,11 +58,6 @@ class CapeClient:
         if 'documentIds' in parameters and not isinstance(parameters['documentIds'], str):
             parameters['documentIds'] = json.dumps(parameters['documentIds'])
         if parameters:
-            if self.session_cookie:
-                r = self.session.get(url, cookies={'session': self.session_cookie})
-            else:
-                r = self.session.get(url)
-        else:
             m = encoder.MultipartEncoderMonitor.from_fields(fields=parameters, encoding='utf-8',
                                                             callback=monitor_callback)
             if self.session_cookie:
@@ -70,6 +65,11 @@ class CapeClient:
                                       headers={'Content-Type': m.content_type})
             else:
                 r = self.session.post(url, data=m, headers={'Content-Type': m.content_type})
+        else:
+            if self.session_cookie:
+                r = self.session.get(url, cookies={'session': self.session_cookie})
+            else:
+                r = self.session.get(url)
 
         if r.status_code == 200 and r.json()['success']:
             return r
