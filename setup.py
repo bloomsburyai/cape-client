594884c7ea3ab800332b4bac4fae44f77af2a08f
Michael Sheldon <mike@bloomsbury.ai>
2018-01-08 11:30:21 +0000

    Add additional metadata for pypi

--- setup.py
+++ setup.py
@@ -7,9 +7,28 @@ setup(
     long_description=DESCRIPTION,
     author='Bloomsbury AI',
     author_email='contact@bloomsbury.ai',
+    license='MIT',
+    keywords='ai nlp natural language question answer chat chatbot bot artificial intelligence',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Communications :: Chat',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Text Processing :: Linguistic',
+    ],
     packages=PACKAGES,
     include_package_data=True,
     package_data={
         '': ['*.*'],
     },
+    python_requires='>=3.5',
+    install_requires=[
+        'requests>=2.18.1',
+        'requests-toolbelt>=0.8.0',
+    ],
 )
