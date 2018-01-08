f08ec22232b44793d4404c69aba608c2bcd410d5
Michael Sheldon <mike@bloomsbury.ai>
2018-01-08 13:08:25 +0000

    Add extra metadata for PyPi

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     name=NAME,
     version=VERSION,
     long_description=DESCRIPTION,
+    description='Client library for the Cape machine reading API'
     author='Bloomsbury AI',
     author_email='contact@bloomsbury.ai',
+    url='https://thecape.ai/',
     license='MIT',
     keywords='ai nlp natural language question answer chat chatbot bot artificial intelligence',
     classifiers=[
